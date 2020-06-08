dd8c69290273562ac37851282ae15b2497fbd895
Alan Li <li.alan011@gmail.com>
2020-06-08 16:50:17 -0700

    attempting to fix bugs

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,9 +5,13 @@ from .auth import AuthError, requires_auth
 bp = Blueprint('main', __name__, '')
 
 
+# @bp.route('/')
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth
+# def main_page():
+#     response = "Hello from a private endpoint! You need to be authenticated to see this."
+#     return jsonify(message=response)
+
 @bp.route('/')
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def main_page():
-    response = "Hello from a private endpoint! You need to be authenticated to see this."
-    return jsonify(message=response)
+def main():
+    return "hello world"
