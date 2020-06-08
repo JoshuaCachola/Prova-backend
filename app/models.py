4ce7fd2ff68d97486f893a7d63b3ab4feab8e57c
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-06-08 19:29:54 -0400

    Add Route model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Route(db.Model):
+    __tablename__ = 'routes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    distance = db.Column(db.Float, nullable=False)
+    average_time = db.Column(db.Integer)
+    best_time = db.Column(db.Integer)
+    coordinates = db.Column(db.Text, nullable=False)
+    creatorId = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
