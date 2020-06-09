2b9d151910abdc8a6fc9c86b072fc99a8b934ef7
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-06-09 17:41:00 -0400

    changes

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from flask_cors import cross_origin
+from ..models import Route
 
 bp = Blueprint('main', __name__, '')
 
@@ -6,3 +8,18 @@ bp = Blueprint('main', __name__, '')
 @bp.route('/')
 def main_page():
     return 'Hello World'
+
+
+@bp.route('/users/<user_id>/routes')
+@cross_origin(headers=["Content-Type", "Authorization"])
+def get_my_routes(user_id):
+    my_routes = Route.query.filter(Route.creatorId == user_id).all()
+    return jsonify(my_routes)
+
+
+@bp.route('/routes', methods=['POST'])
+@cross_origin(headers=["Content-Type", "Authorization"])
+def post_route():
+    data = request.json
+    print(data)
+    return jsonify(data)
