16a98477c496c8473aa1493b0760077454ccc1c2
Alan Li <li.alan011@gmail.com>
2020-06-08 22:19:12 -0700

    change code to main page to say hello world

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,17 +1,8 @@
-from flask import Blueprint, jsonify
-from flask_cors import cross_origin, CORS
-from .auth import AuthError, requires_auth
+from flask import Blueprint
 
 bp = Blueprint('main', __name__, '')
 
 
-# @bp.route('/')
-# @cross_origin(headers=["Content-Type", "Authorization"])
-# @requires_auth
-# def main_page():
-#     response = "Hello from a private endpoint! You need to be authenticated to see this."
-#     return jsonify(message=response)
-
 @bp.route('/')
-def main():
-    return "hello world"
+def main_page():
+    return 'Hello World'
