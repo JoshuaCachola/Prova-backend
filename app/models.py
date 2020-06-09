9e54ac47d418093b6bc4687e3886fc61bb685369
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-06-08 17:05:02 -0700

    added runs model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Run(db.Model):
+    __tablename__ = "runs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    distance = db.Column(db.Float)
+    time = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
+    calories = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    route_id = db.Column(db.Integer, db.ForeignKey('routes'))
+
+    users = db.relationship('User', back_populates='runs')
+    routes = db.relationship('Route', back_populates='runs')
