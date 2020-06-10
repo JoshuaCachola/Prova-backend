5c406dda29031fb93dc996de23bbf6b873207904
Alan Li <li.alan011@gmail.com>
2020-06-10 15:09:27 -0700

    create route for one user

--- app/routes/users.py
+++ app/routes/users.py
@@ -8,12 +8,20 @@ bp = Blueprint('users', __name__, url_prefix="")
 
 @bp.route('/users')
 @cross_origin(headers=["Content-Type", "Authorization"])
-def user():
+def get_users():
     users = User.query.all()
     all_users = []
     for user in users:
         all_users.append(user.to_dict())
     return jsonify(all_users)
+
+
+@bp.route('/users/<user_id>')
+@cross_origin(headers=["Content-Type", "Authorization"])
+def get_user():
+    data = request.json
+    user = User.query.filter_by(email=data['email']).first()
+    return jsonify(user)
 
 
 @bp.route('/users', methods=['POST'])
